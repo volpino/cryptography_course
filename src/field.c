32fd31a10e4315b92658a84d4f8f5f17a1a4370c
volpino <fox91@anche.no>
2012-10-19 15:26:03 +0200

    some functions for testing

--- src/field.c
+++ src/field.c
@@ -1,6 +1,15 @@
 #include "field.h"
 #include <stdio.h>  /* only for debugging */
 
+#define G6_BYTETOBINARYPATTERN "(%d,%d,%d,%d,%d,%d)"
+#define G6_BYTETOBINARY(byte)  \
+  (byte & 0x20 ? 1 : 0), \
+  (byte & 0x10 ? 1 : 0), \
+  (byte & 0x08 ? 1 : 0), \
+  (byte & 0x04 ? 1 : 0), \
+  (byte & 0x02 ? 1 : 0), \
+  (byte & 0x01 ? 1 : 0)
+
 /* Add two numbers in a GF(2^6) finite field */
 uint8_t g6_add(uint8_t a, uint8_t b) {
     return (a ^ b) & 0x3f;
@@ -42,13 +51,34 @@ uint8_t g6_rotate(uint8_t a, int8_t d) {
     return res ^ (tmp % 64);
 }
 
+uint8_t g6_input() {
+    uint8_t a0, a1, a2, a3, a4, a5;
+    scanf("(%c,%c,%c,%c,%c,%c)", &a0, &a1, &a2, &a3, &a4, &a5);
+    return (a0 - '0')*0x20 + (a1 - '0')*0x10 + (a2 - '0')*0x08 + \
+           (a3 - '0')*0x04 + (a4 - '0')*0x02 + (a5 - '0')*0x01;
+}
+
+void g6_print(uint8_t a) {
+    printf(G6_BYTETOBINARYPATTERN, G6_BYTETOBINARY(a));
+}
+
+void field_test() {
+    uint8_t a, b;
+
+    printf("Gimme the A vector: ");
+    a = g6_input();
+    scanf("\n");
+    printf("Gimme the B vector: ");
+    scanf("\n");
+    b = g6_input();
+
+    printf("\nA + B: ");
+    g6_print(g6_add(a, b));
+    printf("\nA * B: ");
+    g6_print(g6_mul(a, b));
+    printf("\n");
+}
+
 int main() {
-    printf("3 + 11 = %d\n", g6_add(3, 11));
-    printf("27 + 12 = %d\n", g6_add(27, 12));
-    printf("3 * 11 = %d\n", g6_mul(3, 11));
-    printf("6 * 5 = %d\n", g6_mul(6, 5));
-    printf("R(0x11, 4) = %d\n", g6_rotate(0x11, 4));
-    printf("R(0x11, 2) = %d\n", g6_rotate(0x11, 2));
-    printf("R(0x11, -4) = %d\n", g6_rotate(0x11, -4));
-    printf("R(0x11, -2) = %d\n", g6_rotate(0x11, -2));
+    field_test();
 }
