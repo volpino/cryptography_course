c131fc5ee90859976844006d319457360aeede67
volpino <fox91@anche.no>
2012-10-19 16:33:19 +0200

    adding lambda

--- src/lib/bunny24/bunny_internals.h
+++ src/lib/bunny24/bunny_internals.h
@@ -1,5 +1,17 @@
 #ifndef _BUNNY_INTERNALS_H_
 #define _BUNNY_INTERNALS_H_
 
+#include "../field.h"
+
+#define bunny_add(a, b) gf_add((a), (b))
+#define bunny_mul(a, b) gf_mul((a), (b), 6, 0x1b)  /* x^4 + x^3 + x + 1 */
+#define bunny_rotate(a, b) gf_rotate((a), (b), 6)
+
+uint8_t lambda[4][4] = {
+    {0x23, 0x3b, 0x38, 0x3d},
+    {0x0d, 0x3c, 0x16, 0x18},
+    {0x03, 0x20, 0x17, 0x37},
+    {0x2c, 0x26, 0x38, 0x13}
+};
 
 #endif
--- src/lib/bunny24/gen_sbox.c
+++ src/lib/bunny24/gen_sbox.c
@@ -1,68 +1,79 @@
-#include "../field.h"
 #include "bunny_internals.h"
 #include <stdio.h>
 
-#define E 0x2;  /* (0,0,0,0,1,0) */
+#define E 0x2  /* (0,0,0,0,1,0) */
+
 
 void SB1() {
     uint8_t i, j, result;
 
-    printf("uint8_t SB1 = {");
+    printf("uint8_t SB1[] = {");
 
-    for (i=0; i<0x40; i++) {
+    for (i=0; i<64; i++) {
         result = i;
         for (j=0; j<62; j++) {
             result = bunny_mul(result, i);
         }
-        printf("%d, ", result);
+        printf("%d%s", result, i < 63 ? ", " : "");
     }
-    printf("}\n");
+    printf("};\n");
 }
 
 
 void SB2() {
     uint8_t i, j, result;
 
-    printf("uint8_t SB2 = {");
+    printf("uint8_t SB2[] = {");
 
-    for (i=0; i<0x40; i++) {
+    for (i=0; i<64; i++) {
         result = i;
-        for (j=0; j<62; j++) {
+        for (j=0; j<5; j++) {
             result = bunny_mul(result, i);
         }
-        printf("%d, ", bunny_add(result, bunny_mul(E, E)));
+        printf("%d%s", result, i < 63 ? ", " : "");
     }
-    printf("}\n");
+    printf("};\n");
 }
 
 
 void SB3() {
     uint8_t i, j, result;
 
-    printf("uint8_t SB1 = {");
+    printf("uint8_t SB3[] = {");
 
-    for (i=0; i<0x40; i++) {
+    for (i=0; i<64; i++) {
         result = i;
-        for (j=0; j<5; j++) {
+        for (j=0; j<17; j++) {
             result = bunny_mul(result, i);
         }
-        printf("%d, ", result);
+        printf("%d%s", result, i < 63 ? ", " : "");
     }
-    printf("}\n");
+    printf("};\n");
 }
 
 
 void SB4() {
     uint8_t i, j, result;
+    uint8_t e2 = bunny_mul(E, E);
 
-    printf("uint8_t SB1 = {");
+    printf("uint8_t SB4[] = {");
 
-    for (i=0; i<0x40; i++) {
+    for (i=0; i<64; i++) {
         result = i;
-        for (j=0; j<17; j++) {
+        for (j=0; j<62; j++) {
             result = bunny_mul(result, i);
         }
-        printf("%d, ", result);
+        printf("%d%s", bunny_add(result, e2), i < 63 ? ", " : "");
     }
-    printf("}\n");
+    printf("};\n");
+}
+
+
+int main() {
+    printf("%d\n", bunny_mul(2,2));
+
+    SB1();
+    SB2();
+    SB3();
+    SB4();
 }
