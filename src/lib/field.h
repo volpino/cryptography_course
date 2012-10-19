70fefff2bff5267fc2da03e739d46a069d45febe
volpino <fox91@anche.no>
2012-10-19 16:13:26 +0200

    field.h updated

--- src/lib/field.h
+++ src/lib/field.h
@@ -3,9 +3,8 @@
 
 #include <stdint.h>
 
-uint8_t g6_add(uint8_t a, uint8_t b);
-uint8_t g6_sub(uint8_t a, uint8_t b);
-uint8_t g6_mul(uint8_t a, uint8_t b);
-uint8_t g6_rotate(uint8_t a, int8_t d);
+uint8_t gf_add(uint8_t a, uint8_t b);
+uint8_t gf_mul(uint8_t a, uint8_t b, uint8_t n_bits, uint8_t poly);
+uint8_t gf_rotate(uint8_t a, int8_t d, uint8_t n_bits);
 
 #endif
