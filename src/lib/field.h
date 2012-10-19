d9691005b48a4b8b227bba16e8abef84c5a554d1
volpino <fox91@anche.no>
2012-10-19 16:02:16 +0200

    style fix

--- src/lib/field.h
+++ src/lib/field.h
@@ -1,5 +1,5 @@
-# ifndef _FIELD_H_
-# define _FIELD_H_
+#ifndef _FIELD_H_
+#define _FIELD_H_
 
 #include <stdint.h>
 
@@ -8,4 +8,4 @@ uint8_t g6_sub(uint8_t a, uint8_t b);
 uint8_t g6_mul(uint8_t a, uint8_t b);
 uint8_t g6_rotate(uint8_t a, int8_t d);
 
-# endif
+#endif
