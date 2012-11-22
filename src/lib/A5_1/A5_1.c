53390d2be15fedf980ec2ad4e583dab018fe15fc
volpino <fox91@anche.no>
2012-11-22 16:48:25 +0100

    fixing existing functions

--- src/lib/A5_1/A5_1.c
+++ src/lib/A5_1/A5_1.c
@@ -5,10 +5,10 @@
 #define KEY_LEN 64
 #define FRAME_LEN 22
 
-void init(lfsr* registers, int len) {
+void init(reg_set_t* reg_set) {
   int i;
-  for (i=0; i<len; i++) {
-    registers[i].state = 0;
+  for (i=0; i<reg_set->num; i++) {
+    (reg_set->registers[i]).state = 0;
   }
 }
 
@@ -30,20 +30,27 @@ void keyloading(reg_set_t* reg_set,
 uint8_t shift_majority_1(reg_set_t* reg_set) {
   int i, count_0 = 0, count_1 = 0;
   uint8_t out_bit, temp, tap, majority;
-  for(i=0; i<reg_set->num; i++) {
-    if (reg_set->registers[i].state & reg_set->taps[i])
-      count_1 ++;
+  for (i=0; i<reg_set->num; i++) {
+    if (reg_set->registers[i].state & reg_set->taps[i])  /* if the tap bit is one */
+      count_1++;
     else
-      count_0 ++;
+      count_0++;
   }
   majority = (count_1 > count_0) ? 1 : 0;
-  out_bit = 0x0;
-  for(i=0; i<reg_set->num; i++) {
+  out_bit = 0;
+  for (i=0; i<reg_set->num; i++) {
+    /* take the value of the tap */
     tap = (reg_set->registers[i].state & reg_set->taps[i]) ? 1 : 0;
-    if (!(tap ^ majority)) {/* if tap agrees with majority */
+
+    if (tap == majority) { /* if tap agrees with majority */
       lfsr_rotate(&(reg_set->registers[i]), &temp, 1);
       out_bit ^= (temp ? 1 : 0);
     }
   }
   return out_bit;
+}
+
+
+void a5_1(uint64_t key, int out_len) {
+
 }
