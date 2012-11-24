db73d741b1de4d94b81a98820548ab9fd1c345e4
volpino <fox91@anche.no>
2012-11-24 09:10:33 +0100

    hash seems to work! Holy crap, that was hard

--- src/lib/sponge_hash/hash.c
+++ src/lib/sponge_hash/hash.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include "../bunny24.h"
 
+
 /* every m element is 20 bits, the last 4 bits are 0 */
 void sponge_hash_internal(b24_t* m, int len_m, b24_t key, uint8_t* out) {
   uint8_t state_byte[3];
   b24_t state;
-  int i, j;
+  int i, j, k;
 
   for (i=0; i<B24_T_LEN; i++) {
     state[i] = 0;
@@ -13,46 +14,96 @@ void sponge_hash_internal(b24_t* m, int len_m, b24_t key, uint8_t* out) {
 
   /* absorbing phase */
   for (i=0; i<len_m; i++) {
+    //printf("1)");
+    //g6_print(state[0]);g6_print(state[1]);g6_print(state[2]);g6_print(state[3]);printf("\n");
     for (j=0; j<B24_T_LEN; j++) {
+      //printf("XOR: "); g6_print(state[j]); g6_print(m[i][j]); printf("\n");
       state[j] ^= m[i][j];
     }
+    //printf("2)");
+    //g6_print(state[0]);g6_print(state[1]);g6_print(state[2]);g6_print(state[3]);printf("\n");
     b24_encrypt(state, key);
+    //printf("3)");
+    //g6_print(state[0]);g6_print(state[1]);g6_print(state[2]);g6_print(state[3]);printf("\n\n\n");
   }
-
 
   /* squeezing phase */
   /* we create the output 40 bits at a time,
      so we need 4 loops to get to 160 bits */
   for (i=0; i<4; i++) {
+    k = i * 5;
+
     b24_to_byte(&state, state_byte, 3);
-    out[i] = state_byte[0];
-    out[i+1] = state_byte[1];
-    out[i+2] = (state_byte[2] & 0xf0);
+
+    out[k] = state_byte[0];
+    out[k+1] = state_byte[1];
+    out[k+2] = (state_byte[2] & 0xf0);
 
     b24_encrypt(state, key);
     b24_to_byte(&state, state_byte, 3);
 
-    out[i+2] = out[i+2] ^ (state_byte[0] >> 4);
-    out[i+3] = (state_byte[0] << 4);
+    out[k+2] = out[k+2] ^ (state_byte[0] >> 4);
 
-    out[i+3] = out[i+3] ^ (state_byte[1] >> 4);
-    out[i+4] = (state_byte[1] << 4);
-    out[i+5] = (state_byte[2] & 0xf0);
+    out[k+3] = state_byte[0] << 4;
+    out[k+3] = out[k+3] ^ (state_byte[1] >> 4);
+
+    out[k+4] = state_byte[1] << 4;
+    out[k+4] = out[k+4] ^ (state_byte[2] >> 4);
+
+    b24_encrypt(state, key);
   }
 }
 
+
+/* This function converts an array of uint8_t to an array of b64_t
+ * where for every b24_t the first 20 bits are filled and the remaining 4
+ * are set to 0.
+ */
+void byte_to_b20(uint8_t* inp, int n, b24_t* out) {
+  int i, j;
+  uint32_t tmp, x1, x2, x3;
+
+  for (i=0, j=0; i<(n + (n % 5 == 0 ? 0 : 1)); i+=5, j+=2) {
+    /* Set 8 + 8 + 4 = 20 bits */
+    x1 = inp[i] << 24;
+    x2 = i+1 < n ? (inp[i+1] << 16) : 0;
+    x3 = i+2 < n ? ((inp[i+2] & 0xF0) << 8) : 0;  /* Take the first 4 bits */
+
+    tmp = x1 + x2 + x3;
+    out[j][0] = (tmp & 0xFC000000) >> 26;
+    out[j][1] = (tmp & 0x03F00000) >> 20;
+    out[j][2] = (tmp & 0x000FC000) >> 14;
+    out[j][3] = (tmp & 0x00003F00) >> 8;
+
+    /* Set the remaining 4 + 8 + 8 = 20 bits */
+    x1 = i+2 < n ? ((inp[i+2] & 0x0F) << 28) : 0;  /* And then the last 4 */
+    x2 = i+3 < n ? (inp[i+3] << 20) : 0;
+    x3 = i+4 < n ? (inp[i+4] << 12) : 0;
+
+    tmp = x1 + x2 + x3;
+    if (tmp != 0) {
+      out[j + 1][0] = (tmp & 0xFC000000) >> 26;
+      out[j + 1][1] = (tmp & 0x03F00000) >> 20;
+      out[j + 1][2] = (tmp & 0x000FC000) >> 14;
+      out[j + 1][3] = (tmp & 0x00003F00) >> 8;
+    }
+  }
+}
+
+
 void sponge_hash(uint8_t* input, int len, uint8_t* out) {
+  int m_len = (len * 8) / 20 + ((len * 8) % 5 == 0 ? 0 : 1);
   b24_t* m = (b24_t*) malloc(len * sizeof(b24_t));
   b24_t key;
   int i;
 
-  byte_to_b24(input, len, m);
+  byte_to_b20(input, len, m);
 
   /* initialize the key to all 1s */
   for (i=0; i<B24_T_LEN; i++) {
-    key[i] = 1;
+    key[i] = 0x3F;
   }
 
-  sponge_hash_internal(m, len, key, out);
+  sponge_hash_internal(m, m_len, key, out);
   free(m);
 }
--- src/lib/sponge_hash/test.c
+++ src/lib/sponge_hash/test.c
@@ -1,6 +1,101 @@
 #include "../sponge_hash.h"
+#include <stdio.h>
+#include <assert.h>
+
+#define BYTETOBINARYPATTERN "%d%d%d%d%d%d%d%d "
+#define BYTETOBINARY(byte)  \
+  (byte & 0x80 ? 1 : 0), \
+  (byte & 0x40 ? 1 : 0), \
+  (byte & 0x20 ? 1 : 0), \
+  (byte & 0x10 ? 1 : 0), \
+  (byte & 0x08 ? 1 : 0), \
+  (byte & 0x04 ? 1 : 0), \
+  (byte & 0x02 ? 1 : 0), \
+  (byte & 0x01 ? 1 : 0)
+
+void byte_print(uint8_t a) {
+  printf(BYTETOBINARYPATTERN, BYTETOBINARY(a));
+}
+
 
 int main() {
-  uint8_t t;
-  
+  int i;
+  uint8_t m[100];
+  uint8_t o[20];
+  /* 1011001100 */
+  m[0] = 0xb3;
+  sponge_hash(m, 1, o);
+  printf("INPUT: %x\n", m[0]);
+
+  printf("OUTPUT: \n");
+  for (i=0; i<20; i++) {
+    byte_print(o[i]);
+  }
+  printf("\n");
+
+  assert(o[0] == 0x38);
+  assert(o[1] == 0x3c);
+  assert(o[2] == 0x4);
+  assert(o[3] == 0xc4);
+  assert(o[4] == 0xce);
+  assert(o[5] == 0x47);
+  assert(o[6] == 0x79);
+  assert(o[7] == 0x45);
+  assert(o[8] == 0xe7);
+  assert(o[9] == 0x90);
+  assert(o[10] == 0x89);
+  assert(o[11] == 0xd);
+  assert(o[12] == 0x8e);
+  assert(o[13] == 0x72);
+  assert(o[14] == 0x77);
+  assert(o[15] == 0xfa);
+  assert(o[16] == 0x68);
+  assert(o[17] == 0xf1);
+  assert(o[18] == 0x5b);
+  assert(o[19] == 0xa3);
+
+  m[0] = 0x47;
+  m[1] = 0xc;
+  m[2] = 0x39;
+  m[3] = 0xcf;
+  m[4] = 0x9a;
+  m[5] = 0xfc;
+  m[6] = 0xc0;
+
+  sponge_hash(m, 7, o);
+
+  printf("\n\nINPUT: \n");
+  for (i=0; i<7; i++) {
+    byte_print(m[i]);
+  }
+  printf("\n");
+
+  printf("OUTPUT: \n");
+  for (i=0; i<20; i++) {
+    byte_print(o[i]);
+  }
+  printf("\n");
+
+  assert(o[0] == 0x63);
+  assert(o[1] == 0x87);
+  assert(o[2] == 0xf1);
+  assert(o[3] == 0xbe);
+  assert(o[4] == 0xb5);
+  assert(o[5] == 0xed);
+  assert(o[6] == 0xb0);
+  assert(o[7] == 0xd);
+  assert(o[8] == 0x6);
+  assert(o[9] == 0x48);
+  assert(o[10] == 0x7e);
+  assert(o[11] == 0x52);
+  assert(o[12] == 0x43);
+  assert(o[13] == 0x84);
+  assert(o[14] == 0x34);
+  assert(o[15] == 0x66);
+  assert(o[16] == 0xb5);
+  assert(o[17] == 0x60);
+  assert(o[18] == 0x84);
+  assert(o[19] == 0x64);
+
+  return 0;
 }
