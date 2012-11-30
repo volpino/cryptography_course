c895f47a997e97dc362f990204cf37700379733e
volpino <fox91@anche.no>
2012-11-30 12:54:51 +0100

    draft of prime.c

---
+++ src/lib/RSA/prime.c
@@ -0,0 +1,15 @@
+#include "../bunny24_prng.h"
+
+
+void generate_random_prime() {
+  // while a suitable prime is not found
+
+    // pick a random q with first and last bits as 1
+
+    // generate p = 2 * q + 1
+
+    // check that both p-1 and q-1 don't have small factors
+    // Note: exclude 2 from primes
+
+    // check that (p-1)/2 is prime (it's ok to do just p/2, integer division)
+}
