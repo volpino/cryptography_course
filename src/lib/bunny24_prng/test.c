36f279fa4811ad8f1709be2287e534b2057aa1ff
Andrea Nodari <andrea.nodari91@gmail.com>
2012-12-03 18:16:29 +0100

    test changed

--- src/lib/bunny24_prng/test.c
+++ src/lib/bunny24_prng/test.c
@@ -1,17 +1,44 @@
 #include "../bunny24_prng.h"
 #include <stdio.h>
 
+//#define N_BIT 2097152
+#define N 16777216
+
+uint8_t out[N];
+int hash[N];
+
+
+int bytes_to_int(uint8_t* bytes) {
+  int i;
+  int res = bytes[0];
+  for (i=1; i<3; i++) {
+    res <<= 8;
+    res += bytes[i];
+  }
+  return res;
+}
+
 int main() {
   int i;
   uint8_t seed[3] = {0x01, 0x02, 0x03};
-  uint8_t out[100];
 
-  bunny24_prng(seed, 3, out, 100);
-
-  for (i=0; i<100; i++) {
-    printf("%x ", out[i]);
+  for (i=0; i<N; i++) {
+    hash[i] = -1;
   }
-  printf("\n");
+
+  printf("It is working, or is it!\n");
+  bunny24_prng(seed, 3, out, N);
+
+  for (i=0; i<N; i+=3) {
+    int tmp = bytes_to_int(&out[i]);
+    if (hash[tmp] != -1) {
+      printf("DLF hash[tmp]=%d\n", hash[tmp]);
+      printf("DLF i=%d\n", i);
+    }
+    else {
+      hash[tmp] = i;
+    }
+  }
 
   return 0;
 }
