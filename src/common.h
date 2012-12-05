9b731359baba85992f76396143402addfe765994
volpino <fox91@anche.no>
2012-12-05 23:24:47 +0100

    bunch of changes, server.c is more or less naive implemented

--- src/common.h
+++ src/common.h
@@ -12,16 +12,22 @@
 #include <stdint.h>
 
 #include <openssl/bn.h>
+#include "lib/bunny24.h"
 #include "lib/bunny24_prng.h"
+#include "lib/A5_1.h"
 #include "lib/rsa.h"
 
 #define MSG_SIZE_MAX 2048
 #define CONNECTION_STRING "Hello!"
 #define CLOSE_CONNECTION_STRING "Bye"
+#define DECRYPTED_STRING "MESSAGE RECEIVED AND DECRYPTED!"
+#define CORRUPTED_STRING "CORRUPTED MESSAGE RECEIVED!"
 #define OK_STRING "OK"
 #define R_SIZE 32
+#define K_SIZE 8
 #define SEED_SIZE 3
-#define RSA_LENGTH 64
+#define RSA_LENGTH 65
+#define HASH_LENGTH 20
 
 void print_buff(u_int8_t *, size_t);
 int open_channel(const char *);
@@ -32,5 +38,8 @@ ssize_t write_msg(int, const u_int8_t *, u_int16_t);
 int read_string(int, const char *);
 int write_OK(int);
 int write_BYE(int);
+
+void decrypt(int cipher_id, uint8_t *c, int c_len, uint8_t *k);
+void encrypt(int cipher_id, uint8_t *c, int c_len, uint8_t *k);
 
 #endif
--- src/server.c
+++ src/server.c
@@ -34,12 +34,15 @@ int main(int argc, char ** argv) {
   char client_nm[256];
   uint8_t rsa_tmp[RSA_LENGTH], rsa_tmp2[RSA_LENGTH];
   ssize_t msg_size;
-  uint8_t * buff;
+  uint8_t *buff;
   uint8_t r[R_SIZE];
   uint8_t seed[SEED_SIZE];
+  uint8_t k[K_SIZE];
+  int block_id, hash_id, public_id, c_block_id, c_hash_id, c_public_id;
+  int k_len;
   FILE *fp;
 
-  BIGNUM *bn_n, *bn_e, *bn_d, *bn_client_e, *bn_client_n, *bn_r;
+  BIGNUM *bn_n, *bn_e, *bn_d, *bn_client_e, *bn_client_n, *bn_r, *bn_r1;
 
   bn_n = BN_new();
   bn_e = BN_new();
@@ -47,6 +50,7 @@ int main(int argc, char ** argv) {
   bn_client_e = BN_new();
   bn_client_n = BN_new();
   bn_r = BN_new();
+  bn_r1 = BN_new();
 
   /* Mandatory arguments */
   if (!argv[1] || !argv[2] || !argv[3]) {
@@ -94,13 +98,17 @@ int main(int argc, char ** argv) {
     }
 
     /* DECRYPT c using (s_prk,n) -> r' = c^s_prk mod n */
-    /*rsa_decrypt(bn_d, buff, msg_size);*/
+    buff[msg_size] = '\0';
+    BN_hex2bn(&bn_r, buff);
+    rsa_decrypt(bn_r, bn_d, bn_n);
+    buff = BN_bn2hex(bn_r);
 
     /* SEND r' to C */
     if ((write_msg(sc_fifo_fd, buff, msg_size)) < 0) {
       fprintf(stderr, "Error while sending C to the client...\n");
       goto next;
     }
+    OPENSSL_free(buff);
 
     /* Client authentication */
     /* READ client name nm of C */
@@ -109,7 +117,8 @@ int main(int argc, char ** argv) {
       goto next;
     }
 
-    /* GET the public rsa keys of the possible clients associated to each name, (names[],c_puk[],n[]) from "client_folder/clients_rsa_public_keys.txt" */
+    /* GET the public rsa keys of the possible clients associated to each name,
+     * (names[],c_puk[],n[]) from "client_folder/clients_rsa_public_keys.txt" */
     /* EXTRACT from (names[],c_puk[],n[]) the pair (c_puk[i],n[i]) where names[i] = nm */
     if ((fp = fopen("server_folder/clients_rsa_public_keys.txt", "r")) == NULL) {
       fprintf(stderr, "Error while getting clients RSA public keys...\n");
@@ -120,6 +129,7 @@ int main(int argc, char ** argv) {
       fscanf(fp, "%64s %64s %64s", client_nm, rsa_tmp, rsa_tmp2);
       if (strcmp(client_nm, buff) == 0) {
         done = 1;
+        break;
       }
     }
     if (done == 0) {
@@ -136,26 +146,137 @@ int main(int argc, char ** argv) {
     for (i=0; i<SEED_SIZE; i++) {
       fscanf(fp, "%c", &(seed[i]));
     }
+    fclose(fp);
     bunny24_prng(seed, SEED_SIZE, NULL, r, R_SIZE);
+    BN_bin2bn(r, R_SIZE, bn_r);
 
-    /* ENCRYPT r using s_puk[i] -> c = r^s_puk[i] mod n[i] */
-
+    /* ENCRYPT r using c_puk[i] -> r' = r^c_puk[i] mod n[i] */
+    rsa_encrypt(bn_r, bn_client_e, bn_client_n);
 
     /* WRITE c to C */
-    /* ... */
+    buff = BN_bn2hex(bn_r);
+    if ((write_msg(sc_fifo_fd, buff, msg_size)) < 0) {
+      fprintf(stderr, "Error while sending C to the client...\n");
+      goto next;
+    }
+    OPENSSL_free(buff);
+
+    /* READ r' from C */
+    if ((msg_size = read_msg(cs_fifo_fd,&buff)) < 0) {
+      fprintf(stderr, "error while getting r' from the client...\n");
+      goto next;
+    }
+
+    /* CHECK that r = r' */
+    BN_bin2bn(r, R_SIZE, bn_r);
+    BN_hex2bn(&bn_r1, buff);
+    if (BN_cmp(bn_r, bn_r1) != 0) {
+      fprintf(stderr, "Error, r and r' mismatch!\n");
+      goto next;
+    }
 
     /* Negotiation of the cipher suite */
-    /* ... */
+    /* READ list from C */
+    if ((msg_size = read_msg(cs_fifo_fd,&buff)) < 0) {
+      fprintf(stderr, "error while getting the list from the client...\n");
+      goto next;
+    }
+    buff[msg_size] = '\0';
+    sscanf(buff, "%d %d %d", &c_block_id, &c_hash_id, &c_public_id);
+
+    /* Search for the cipher suite in the file */
+    if ((fp = fopen("server_folder/server_cipher_suite_list.txt", "r")) == NULL) {
+      fprintf(stderr, "Error while getting the cipher list...\n");
+      goto next;
+    }
+    done = 0;
+    while (!feof(fp)) {
+      fscanf(fp, "%d %d %d", &block_id, &hash_id, &public_id);
+      if (c_block_id == block_id && c_hash_id == hash_id &&
+          c_public_id == public_id) {
+        done = 1;
+        break;
+      }
+    }
+    if (done == 0) {
+      fprintf(stderr, "Error: unrecognized client\n");
+      goto next;
+    }
+    fclose(fp);
 
     /* Negotiation of the private key */
-    /* ... */
+
+    /* CREATE a pseudo-random key */
+    fp = fopen("/dev/random", "r");
+    for (i=0; i<SEED_SIZE; i++) {
+      fscanf(fp, "%c", &(seed[i]));
+    }
+    fclose(fp);
+    bunny24_prng(seed, SEED_SIZE, NULL, k, K_SIZE);
+    if (block_id == 1) {
+      k_len = 3;
+    }
+    else {
+      k_len = K_SIZE;
+    }
+    BN_bin2bn(k, k_len, bn_r);
+
+    /* ENCRYPT key */
+    rsa_encrypt(bn_r, bn_client_e, bn_client_n);
+
+    /* WRITE k to C */
+    buff = BN_bn2hex(bn_r);
+    if ((write_msg(sc_fifo_fd, buff, msg_size)) < 0) {
+      fprintf(stderr, "Error while sending C to the client...\n");
+      goto next;
+    }
+    OPENSSL_free(buff);
 
     /* Encrypt communication */
-    /* ... */
+    if ((msg_size = read_msg(cs_fifo_fd,&buff)) < 0) {
+      fprintf(stderr, "error while reading message from the client...\n");
+      goto next;
+    }
 
-    /* Disconnection */
-    /* ... */
+    /* XXX: HOW TO GET C, G?*/
 
+    /* Decrypt C */
+    decrypt(block_id, c, c_len, k);
+
+    /* CHECK H(M') = G */
+    sponge_hash(c, c_len, g1);
+
+    done = 1;
+    for (i=0; i<HASH_LENGTH; i++) {
+      if (g[i] != g1[i]) {
+        done = 0;
+      }
+    }
+
+    /* If the check fails print error message */
+    if (done == 0) {
+      if ((write_msg(sc_fifo_fd, CORRUPTED_STRING, strlen(CORRUPTED_STRING))) < 0) {
+        fprintf(stderr, "Error while writing to the client...\n");
+        goto next;
+      }
+    }
+
+    /* PUT M' on a file */
+    if (fopen("server_message", "w") == NULL) {
+        fprintf(stderr, "Error while saving message...\n");
+        fclose(fp);
+        goto next;
+    }
+    fprintf(fp, "%s", c);
+    fclose(fp);
+
+    /* WRITE ok message to C */
+    if ((write_msg(sc_fifo_fd, DECRYPTED_STRING, strlen(DECRYPTED_STRING))) < 0) {
+      fprintf(stderr, "Error while writing C to the client...\n");
+      goto next;
+    }
+
+  /* Disconnection */
   next:
     /* Close current connection */
     fprintf(stderr,"Closing connection...\n");
@@ -173,6 +294,7 @@ int main(int argc, char ** argv) {
   BN_free(bn_client_n);
   BN_free(bn_client_e);
   BN_free(bn_r);
+  BN_free(bn_r1);
 
   exit(0);
 }
